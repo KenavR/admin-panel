a5a130adf49de4b872f2a6042635732809c1eb87
KenavR <kenavr@gmail.com>
2018-10-21 20:52:12 +0200

    Removed build directory, updated anthill.json file

--- build/install.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function install(frictionCoreApi, config) {
-    if (config === void 0) { config = { path: "/friction" }; }
-    var registerStatic = frictionCoreApi.extensions["friction-express"].registerStatic;
-    console.log(" register static ");
-    registerStatic("/", "d:/projects/friction/extensions/gui/build");
-}
-exports.install = install;
